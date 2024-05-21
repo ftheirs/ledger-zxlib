588c16fb724ff1050f2b079be0825b346c6de0fb
ftheirs <fntheirs@gmail.com>
2024-05-21 15:10:50 -0300

    TEST CI

--- app/ui/view.h
+++ app/ui/view.h
@@ -35,7 +35,7 @@
 
 // By default our builds are not production ready
 // Unless we specificaly define PRODUCTION_BUILD to 1
-#if (PRODUCTION_BUILD == 0)
+#if defined(PRODUCTION_BUILD) && (PRODUCTION_BUILD == 0)
 #undef MENU_MAIN_APP_LINE1
 #undef MENU_MAIN_APP_LINE2
 
